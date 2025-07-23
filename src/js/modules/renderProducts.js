9af0f7b2e6b9e2bdb414c6f900c82dbf93f258b7
Emmanuel Martínez <emmanuel1407@hotmail.com>
2025-07-23 10:45:30 -0300

    Se mejoran las funciones de renderizar los productos para una mayor comprensión.

--- src/js/modules/renderProducts.js
+++ src/js/modules/renderProducts.js
@@ -8,31 +8,15 @@ export function renderProducts(products) {
         productsContainer.innerHTML = ''; // Limpiar el contenedor antes de agregar productos destacados
 
         products.forEach(product => {
-            const productCard = document.createElement('div');
-            productCard.classList.add('card');
-            productCard.setAttribute('data-id', product.id); // Agregar atributo data-id para identificar el producto
-
-            productCard.innerHTML = `
-            <img class="card__image" src="${product.image}.jpg" alt="Imagen de ${product.title}">
-            <h4 class="card__title">${product.title}</h4>
-            <span class="card__code">${product.id}</span>
-            <p class="card__description">${product.descripcion}</p>
-            <span class="card__price">$${product.price.toLocaleString()}</span>
-            <div class="card__actions">
-                <button class="button button--remove">Quitar del carrito</button>
-                <span class="card__counter">0</span>
-                <button class="button button--add">Agregar al carrito</button>
-            </div>
-            `;
-
-            productsContainer.appendChild(productCard);
+            const card = renderProduct(product);
+            console.log(product)
+            productsContainer.appendChild(card);
         });
 
     }catch (error) {
         console.error("Error al renderizar productos:", error);
     }
 }
-
 
 // featured = true
 export function renderFeaturedProducts(products) {
@@ -44,18 +28,108 @@ export function renderFeaturedProducts(products) {
 
         products.forEach(product => {
             if (!product.featured) return; // Solo renderizar productos destacados
-            const productCard = document.createElement('div');
-            productCard.classList.add('card');
-
-            productCard.innerHTML = `
-            <img class="card__image" src="${product.image}.jpg" alt="Imagen de ${product.title}">
-            <h4 class="card__title">${product.title}</h4>
-            <span class="card__price">$${product.price.toLocaleString()}</span>
-            `;
-
-            productsContainer.appendChild(productCard);
+            const card = renderFeaturedProduct(product);
+            console.log(product)
+            productsContainer.appendChild(card);
         });
     }catch (error) {
         console.error("Error al renderizar productos destacados:", error);
     }
+}
+
+
+function renderProduct(product) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.setAttribute('data-id', product.id); // Agregar atributo data-id para identificar el producto
+
+    const img = document.createElement('img');
+    img.src = `${product.image}.jpg`;
+    img.alt = `Imagen de ${product.title}`;
+    img.classList.add('card__image');
+
+    const title = document.createElement('h4');
+    title.textContent = product.title;
+    title.classList.add('card__title');
+
+    const code = document.createElement('span');
+    code.textContent = product.id;
+    code.classList.add('card__code');
+
+    const description = document.createElement('p');
+    description.textContent = product.descripcion;
+    description.classList.add('card__description');
+
+    const seeMoreButton = document.createElement('button');
+    seeMoreButton.textContent= 'Ver más';
+    seeMoreButton.classList.add('button--see-more');
+
+    const price = document.createElement('span');
+    price.textContent = `$${product.price.toLocaleString()}`;
+    price.classList.add('card__price');
+
+
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'Quitar del carrito';
+    removeButton.classList.add('button', 'button--remove');
+
+    const addButton = document.createElement('button');
+    addButton.textContent = 'Agregar del carrito';
+    addButton.classList.add('button', 'button--add');
+
+    const counter = document.createElement("span");
+    counter.textContent = "0";
+    counter.classList.add("card__counter");
+
+    const actions = document.createElement('div');
+    actions.classList.add('card__actions');
+    actions.appendChild(removeButton);
+    actions.appendChild(counter);
+    actions.appendChild(addButton);
+
+    card.appendChild(img);
+    card.appendChild(title);
+    card.appendChild(code);
+    card.appendChild(description);
+    card.appendChild(seeMoreButton);
+    card.appendChild(price);
+    card.appendChild(actions);
+
+    return card;
+}
+
+
+function renderFeaturedProduct(product) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.setAttribute('data-id', product.id); // Agregar atributo data-id para identificar el producto
+
+    const img = document.createElement('img');
+    img.src = `${product.image}.jpg`;
+    img.alt = `Imagen de ${product.title}`;
+    img.classList.add('card__image');
+
+    const title = document.createElement('h4');
+    title.textContent = product.title;
+    title.classList.add('card__title');
+
+    const code = document.createElement('span');
+    code.textContent = product.id;
+    code.classList.add('card__code');
+
+    const description = document.createElement('p');
+    description.textContent = product.descripcion;
+    description.classList.add('card__description');
+
+    const price = document.createElement('span');
+    price.textContent = `$${product.price.toLocaleString()}`;
+    price.classList.add('card__price');
+
+    card.appendChild(img);
+    card.appendChild(title);
+    card.appendChild(code);
+    card.appendChild(description);
+    card.appendChild(price);
+
+    return card;
 }
