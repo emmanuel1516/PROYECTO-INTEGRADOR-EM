d64653da35e40fa834bc2bce789248395cff6b98
Emmanuel Martínez <emmanuel1407@hotmail.com>
2025-07-23 11:49:24 -0300

    Agrego evento para cerrar el nav en responsive si hago click fuera del navbar o burger

--- src/js/modules/navbarToggle.js
+++ src/js/modules/navbarToggle.js
@@ -7,5 +7,16 @@ export function toggleNavbar() {
         navBar.classList.toggle("navbar--active");
     });
 
+    // Cerrar el navbar si se hace clic fuera
+    document.addEventListener("click", (event) => {
+        const clickedInsideNavbar = navBar.contains(event.target);
+        const clickedToggleButton = toggleButton.contains(event.target);
+
+        if (!clickedInsideNavbar && !clickedToggleButton) {
+            navBar.classList.remove("navbar--active");
+        }
+    });
 
 }
+
+
